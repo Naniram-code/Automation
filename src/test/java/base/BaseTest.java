eced14a9834193b9c27d014da323c2a60c4c80f2
Naniram-code <NaniramBhujel@gmail.com>
2023-03-08 06:53:55 -0500

    all

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -15,6 +15,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class BaseTest {
+    //jgjgjggjgjjgjjjgjgj
 
     public RequestSpecification requestSpecification;
     public AssertActions assertActions;
